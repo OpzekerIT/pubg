261e323d302566bfd7b79582218036c5eb5ff9ba
Lanta <thijs@opzeker.it>
2023-09-21 10:22:19 +0200

    testps1

---
+++ ps1/test.ps1
@@ -0,0 +1,23 @@
+﻿
+$playername = 'Lanta01'
+$headers = @{
+    'accept'        = 'application/vnd.api+json'
+    'Authorization' = 'YOURAPIKEY'
+}
+$playerinfo = Invoke-RestMethod -Uri "https://api.pubg.com/shards/steam/players?filter[playerNames]=$playername" -Method GET -Headers $headers
+$playedid = $playerinfo.data.id
+$seasons = Invoke-RestMethod -Uri 'https://api.pubg.com/shards/steam/seasons' -Method GET -Headers $headers
+
+$season = ($seasons.data | Where-Object { $_.attributes.isCurrentSeason -eq $true }).id
+
+$match_array = @()
+
+
+$seasonstats = Invoke-RestMethod -Uri "https://api.pubg.com/shards/steam/players/$playedid/seasons/$season`?filter[gamepad]=false" -Method GET -Headers $headers
+$pubgmatches = $seasonstats.data.relationships.matchesSquad.data.id
+
+foreach ($match in $pubgmatches) {
+    Write-Output "checking $Match"
+    $match_array += Invoke-RestMethod -Uri "https://api.pubg.com/shards/steam/matches/$Match" -Method GET -Headers $headers
+
+}
