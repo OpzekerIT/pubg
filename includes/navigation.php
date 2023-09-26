b49e4d3e9ebfde82e4b72397b22552f072915fde
Lanta <thijs@opzeker.it>
2023-09-26 15:22:51 +0200

    last

--- includes/navigation.php
+++ includes/navigation.php
@@ -5,7 +5,7 @@
         <li><a href="user_stats.php">User Stats</a></li>
         <li><a href="topstats.php">Top10</a></li>
         <li><a href="topstatsavg.php">Match % T10</a></li>
-        <li><a href="latestmatches.php">Last 10 Matches</a></li>
+        <li><a href="latestmatches.php">Last Matches</a></li>
         <li><a href="last_stats.php">Last 14 days %</a></li>
 
     </ul>
