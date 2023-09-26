886e5ebded9b13b7eb5583717ed4d9130e22e8d0
Lanta <thijs@opzeker.it>
2023-09-26 20:25:36 +0200

    d

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
-        <li><a href="index.php">Home</a></li>
+        <li><a href="index.php">Home111</a></li>
         <li><a href="clan_stats.php">Clan Stats</a></li>
         <li><a href="user_stats.php">User Stats</a></li>
         <li><a href="topstats.php">Top10</a></li>
