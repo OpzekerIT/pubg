93980bef8fefefbbc4b11cc452cd30a7e29ccf24
Lanta <thijs@opzeker.it>
2023-10-03 15:01:33 +0200

    size

--- last_stats.php
+++ last_stats.php
@@ -101,7 +101,7 @@ error_reporting(E_ALL);
                     <td>$matches</td>
                     <td>$wins</td>
                     <td>$deaths</td>
-                    <td><img src='$imagePath' alt='Change Indicator' style='vertical-align: right;' width='25' height='25'/> $change </td>
+                    <td><img src='$imagePath' alt='Change Indicator' style='vertical-align: right;' width='15' height='15'/> $change </td>
                     
                 </tr>";
                 }
