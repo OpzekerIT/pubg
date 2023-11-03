55213c2797dcd3c2c99106a0b79960dde3a2c613
Thijs Stobbelaar <thijs.stobbelaar@rws.nl>
2023-11-03 21:57:40 +0100

    banner

--- topstats.php
+++ topstats.php
@@ -10,7 +10,9 @@ error_reporting(E_ALL);
 <body>
 
 <?php include './includes/navigation.php'; ?>
-
+<header>
+    <img src="./images/banner2.png" alt="banner" class="banner">
+    </header>
 <main>
     <section>
         <h2>User Stats</h2>
