8c1f6c7ca92532992f603ad8d760619a19f423a8
Lanta <thijs@opzeker.it>
2023-11-03 22:11:46 +0100

    border remove

--- index.php
+++ index.php
@@ -36,7 +36,7 @@ $lastMatches = array_slice($allMatches, 0, 8);
     <section>
         <h2>Latest Matches</h2>
 
-        <table border="1">
+        <table>
     <thead>
         <tr>
             <!-- <th>Match Date</th> -->
