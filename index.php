91993cf685c835caf6b7f6b61df83a5c924682a2
Thijs Stobbelaar <thijs.stobbelaar@rws.nl>
2023-11-03 21:30:28 +0100

    head as include

--- index.php
+++ index.php
@@ -24,14 +24,7 @@ $lastMatches = array_slice($allMatches, 0, 8);
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <title>DTCH - PUBG Clan</title>
-    <link rel="stylesheet" href="./includes/styles.css">
-</head>
+<?php include './includes/head.php'; ?>
 <body>
 <?php include './includes/navigation.php'; ?>
 
