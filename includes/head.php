2941af9c49b6c4b1fe2bb04daef1dc505b2864a0
Lanta <thijs@opzeker.it>
2023-11-03 21:30:28 +0100

    head as include

---
+++ includes/head.php
@@ -0,0 +1,10 @@
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DTCH - PUBG Clan</title>
+    <link rel="stylesheet" href="./includes/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="./lib/sorttable.js"></script>
+</head>
