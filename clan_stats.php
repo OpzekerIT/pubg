85cc25173bf0281e192c3f6312841f24a5a67d74
Lanta <thijs@opzeker.it>
2023-11-03 21:35:09 +0100

    test

--- clan_stats.php
+++ clan_stats.php
@@ -6,13 +6,7 @@ error_reporting(E_ALL);
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DTCH - PUBG Clan - Clan Stats</title>
-    <link rel="stylesheet" href="./includes/styles.css">
-</head>
+<?php include './includes/head.php'; ?>
 <body>
 
 <?php include './includes/navigation.php'; ?>
