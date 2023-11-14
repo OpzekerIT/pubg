4ed778fe5610e26b8d3815ec28dffd30a1d9a13b
gr00tie <rutgergroot@gmail.com>
2023-11-14 13:45:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -84,9 +84,7 @@ $lastMatches = array_slice($allMatches, 0, 8);
 </table>
 
       
-        <a href="https://discord.gg/wMXsB3ZmNA" target="_blank" rel="noopener noreferrer" title="Join us on our Discord">
-            <img src="./media/discordlogo.png" alt="Discord Logo" class="discord-logo">
-        </a>
+
     </section>
 </main>
 
