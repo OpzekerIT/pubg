bdded9b74bda30b175979f7121f29116c43271d5
Lanta <thijs@opzeker.it>
2023-11-14 21:34:39 +0100

    sortable

--- matchinfo.php
+++ matchinfo.php
@@ -37,7 +37,7 @@ $lastMatches = array_slice($allMatches, 0, 8);
         <section>
             <h2>Latest Matches</h2>
 
-            <table class='sortable'>
+            <table border='1' class='sortable'>
                 <thead>
                     <tr>
                         <!-- <th>Match Date</th> -->
